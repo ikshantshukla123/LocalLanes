bd47fd495e86d19697b1789db12a54aec986e545
ikshantshukla123 <ikshankshukla44@gmail.com>
2025-08-31 16:10:03 +0530

    utils

---
+++ src/utils/Category.js
@@ -0,0 +1,13 @@
+// src/utils/Category.js
+
+// Define categories for spots
+export const SPOT_CATEGORIES = [
+  "Cafe",
+  "Restaurant",
+  "Bar",
+  "Library",
+  "Park",
+  "Mall",
+  "Gym",
+  "Theatre",
+];
